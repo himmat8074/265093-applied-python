bed223924c7cec28670e500509350978eb7ff67f
beinghsr <himmatrajput3025@gmail.com>
2021-04-17 16:56:55 +0530

    Added opertion in print

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 # program to check the IDE compatibilty for python
 print("Hello world")
+
+#using print for simple operations
+print(5-4+3)
+print((5*2)/5)
+
+#producing a float by operation on a float and a integer
+print(2 * 6.1)
+print(1+2+3+4.0+5)
