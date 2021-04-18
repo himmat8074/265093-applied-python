33876eee464fa0c8951741ee3d21f9c9fa71a762
himmat8074 <himmat.8074@gmail.com>
2021-04-18 12:33:29 +0530

    intialised day2

---
+++ applied-python/Day1.py
@@ -0,0 +1,50 @@
+print("------------------------\n")
+
+# program to check the IDE compatibilty for python
+print("Hello world")
+print("------------------------\n")
+
+#using print for simple operations
+print(5-4+3)
+print((5*2)/5)
+print("------------------------\n")
+
+#producing a float by operation on a float and a integer
+print(2 * 6.1)
+print(1+2+3+4.0+5)
+print("------------------------\n")
+
+#using exponential 
+print(5**2)
+print(4**3**2)
+print("------------------------\n")
+
+#square root also can be determined using exponential
+print(9**(1/2)) 
+print(8**(1/3))
+print("------------------------\n")
+
+#to determine quotient and reminder of a division 
+#for quotient we use //
+print(20//6)
+#for remainder we use %
+print(20%6)
+print("------------------------\n")
+
+#printing strings
+print("this is simple string")
+print("this is string with \" escape character \" demo")
+print("this is string \n to demo new line")
+print(""" this is 
+to demo 
+multiline 
+string using 
+three quotes""")
+print("this is demo " + 'for concatination')
+print("------------------------\n")
+
+#string operation
+# op -> oneoneone and 22222 
+print("one" * 2)
+print(5 * '2')
+print("------------------------\n")
---
+++ applied-python/Day2.py
@@ -0,0 +1,24 @@
+#to determine quotient and reminder of a division 
+#for quotient we use //
+print(20//6)
+#for remainder we use %
+print(20%6)
+print("------------------------\n")
+
+#printing strings
+print("this is simple string")
+print("this is string with \" escape character \" demo")
+print("this is string \n to demo new line")
+print(""" this is 
+to demo 
+multiline 
+string using 
+three quotes""")
+print("this is demo " + 'for concatination')
+print("------------------------\n")
+
+#string operation
+# op -> oneoneone and 22222 
+print("one" * 2)
+print(5 * '2')
+print("------------------------\n")
